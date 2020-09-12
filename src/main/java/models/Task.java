54bbf84ea8c3c09236f1331e8da6346ce3645f97
Ken-muigai <kenmuigai980@gmail.com>
2020-09-12 20:54:37 +0300

    initial DAO set up and testing

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -2,6 +2,7 @@ package models;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Task {
 
@@ -15,21 +16,38 @@ public class Task {
         this.description = description;
         this.completed = false;
         this.createdAt = LocalDateTime.now();
-        instances.add(this);
-        this.id = instances.size();
     }
 
-    public String getDescription() {
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Task)) return false;
+        Task task = (Task) o;
+
+        return getCompleted() == task.getCompleted() && getId() == task.getId() && Objects.equals(getDescription(), task.getDescription());
+    };
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(getDescription(), getId(), getCompleted());
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed){
+        this.completed = completed;
+    }
+
+    public int setId() {
+        return id;
+    }
+
+    public String getDescription(){
         return description;
     }
 
-    public static ArrayList<Task> getAll(){
-        return instances;
-    }
-
-    public static void clearAllTasks(){
-        instances.clear();
-    }
 
     public boolean getCompleted(){
         return this.completed;
@@ -39,19 +57,21 @@ public class Task {
         return createdAt;
     }
 
-    public int getId() {
+    public int getId(){
         return id;
     }
 
-    public static Task findById(int id){
-        return instances.get(id-1); //why minus 1? See if you can figure it out.
-    }
 
-    public void update(String content) {
-        this.description = content;
-    }
 
-    public void deleteTask(){
-        instances.remove(id-1); //same reason
-    }
+//    public static Task findById(int id){
+//        return instances.get(id-1); //why minus 1? See if you can figure it out.
+//    }
+//
+//    public void update(String content) {
+//        this.description = content;
+//    }
+//
+//    public void deleteTask(){
+//        instances.remove(id-1); //same reason
+//    }
 }
--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,7 +1,7 @@
 SET MODE PostgreSQL;
 
-CREATE TABLE IF NOT EXISTS stories{
+CREATE TABLE IF NOT EXISTS stories(
  id in PRIMARY KEY auto_increment;
  description VARCHAR;
  complete BOOLEAN;
-}
+);
